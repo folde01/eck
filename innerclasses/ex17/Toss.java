66407e6a6ba1295b1087912d1e292e606123b5fa
Jesse <folde01@dcs.bbk.ac.uk>
2014-09-13 23:01:48 +0100

    inner classes ex 17 done

---
+++ innerclasses/ex17/Toss.java
@@ -0,0 +1,33 @@
+interface Tossable { 
+  int toss();
+}
+interface TossableFactory { 
+  Tossable getTossable(); 
+}
+class Coin implements Tossable { 
+  public int toss() { 
+    return 2;
+  }
+  public static TossableFactory factory = new TossableFactory() { 
+    public Tossable getTossable() { return new Coin(); }
+  };
+}
+class Dice implements Tossable { 
+  public int toss() { 
+    return 12;
+  }
+  public static TossableFactory factory = new TossableFactory() { 
+    public Tossable getTossable() { return new Dice(); }
+  };
+}
+
+public class Toss { 
+  static void toss(TossableFactory factory) { 
+    Tossable t = factory.getTossable();
+    System.out.println("Toss: " + t.toss());
+  }
+  public static void main(String[] args) { 
+    toss(Dice.factory);
+    toss(Coin.factory);
+  }
+}
