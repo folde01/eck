046c165cba972940c43510e7f7209bd5a47fbfd5
Jesse <folde01@dcs.bbk.ac.uk>
2014-09-23 17:33:14 +0100

    hyo ex 21 done

--- holdingYourObjects/ex21/CountEachWord.java
+++ holdingYourObjects/ex21/CountEachWord.java
@@ -5,12 +5,22 @@ import net.mindview.util.*;
 public class CountEachWord {
   public static void main(String[] args) {
     Map<String, Integer> map = new HashMap<String, Integer>();
+    List<String> list = new ArrayList<String>();
     for (String w : new TextFile("UniqueWords.java", "\\W+")) {
       Integer numw = map.get(w);
+      list.add(w);
       map.put(w, numw == null ? 1 : numw + 1);
     }
 
     System.out.println(map);
+
+    Collections.sort(list,String.CASE_INSENSITIVE_ORDER);
+    System.out.println(list);
+
+    for (String w : new LinkedHashSet<String>(list))
+      System.out.println(w + ": " + map.get(w));
+
+
 
 
   }
