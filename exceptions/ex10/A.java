3941e4930b09233888e3ea554e895a118d95902c
Jesse <folde01@dcs.bbk.ac.uk>
2014-09-25 17:41:43 +0100

    ex10 exceptions done

---
+++ exceptions/ex10/A.java
@@ -0,0 +1,27 @@
+class A { 
+  static void f() throws MyException2 { 
+    try { 
+      g();
+    } catch(MyException1 e) { 
+      e.printStackTrace();
+      throw new MyException2("from f()");
+    }
+  }
+  static void g() throws MyException1 { 
+    throw new MyException1("from g()");
+  }
+  public static void main(String[] args) { 
+    try { 
+      f();
+    } catch(MyException2 e) { 
+      e.printStackTrace();
+    }
+  }
+}
+
+class MyException1 extends Exception { 
+  MyException1(String msg) { super(msg); }
+}
+class MyException2 extends Exception { 
+  MyException2(String msg) { super(msg); }
+}
