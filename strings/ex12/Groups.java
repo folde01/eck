ae2d56e066b6745fb0e7bb9c4ea19d22cd9923d8
Jesse <folde01@dcs.bbk.ac.uk>
2014-09-27 19:42:35 +0100

    ex12 strings done

---
+++ strings/ex12/Groups.java
@@ -0,0 +1,33 @@
+//: strings/Groups.java
+import java.util.*;
+import java.util.regex.*;
+import static net.mindview.util.Print.*;
+
+public class Groups {
+  static public final String POEM =
+    "Twas brillig, and the slithy toves\n" +
+    "Did gyre and gimble in the wabe.\n" +
+    "All mimsy were the borogoves,\n" +
+    "And the mome raths outgrabe.\n\n" +
+    "Beware the Jabberwock, my son,\n" +
+    "The jaws that bite, the claws that catch.\n" +
+    "Beware the Jubjub bird, and shun\n" +
+    "The frumious Bandersnatch.";
+  public static void main(String[] args) {
+    Set<String> matches = new LinkedHashSet<String>();
+    Matcher m = Pattern.compile("(\\b[\\w&&[^A-Z]]\\w*)").matcher(POEM);
+    while(m.find())
+      matches.add(m.group());
+    for (String match : matches) 
+      System.out.println("[" + match + "]");
+  }
+} /* Output:
+[the slithy toves][the][slithy toves][slithy][toves]
+[in the wabe.][in][the wabe.][the][wabe.]
+[were the borogoves,][were][the borogoves,][the][borogoves,]
+[mome raths outgrabe.][mome][raths outgrabe.][raths][outgrabe.]
+[Jabberwock, my son,][Jabberwock,][my son,][my][son,]
+[claws that catch.][claws][that catch.][that][catch.]
+[bird, and shun][bird,][and shun][and][shun]
+[The frumious Bandersnatch.][The][frumious Bandersnatch.][frumious][Bandersnatch.]
+*///:~
